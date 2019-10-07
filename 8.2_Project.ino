d93147b0c729e1ca63ebbedc49980ccb86e46bc0
SmHamza <52901691+SmHamza@users.noreply.github.com>
2019-10-08 00:36:01 +1100

    Update 8.2_Project.ino

--- 8.2_Project.ino
+++ 8.2_Project.ino
@@ -1,3 +1,4 @@
+//Libraries
 #include <DHT.h>
 
 //Constants
@@ -6,14 +7,16 @@
 DHT dht(DHTPIN, DHTTYPE); // Initialize DHT sensor for normal 16mhz Arduino
 
 float temp; 
+int freq;
 int i;
 int j;
 int lightsensor = A0;
 int light;
-int freq;
 bool enter;
 bool motionpresent;
 bool light_on;
+bool motion1;
+bool motion2;
 
 void setup() {
   // put your setup code here, to run once:
@@ -33,100 +36,126 @@ void setup() {
 }
 
 void entercheck(){
-  
-  if (digitalRead(2)==HIGH){
-    Serial.print("Checking entering, ");
+  motion1=false;
+  for (i=0; i<300; i++){
+    if (digitalRead(2)==HIGH){
+      motion1=true;
+      i=300;
+    }
+  }
+  if (motion1){
     for (i=0;i<1000; i++){
       if (digitalRead(3)==HIGH){
         enter=true; 
         i=1000;
       }
-      delay(1);
+      delay(2);
     }
-    
   }
 }
 void exitcheck(){
- if (digitalRead(3)==HIGH){
-    Serial.print("Checking exiting, ");
+  motion2=false;
+  for (i=0; i<300; i++){
+    if (digitalRead(3)==HIGH){
+      motion2=true;
+      i=300;
+    }
+  }
+  if (motion2){
     for (i=0;i<1000; i++){ 
       if (digitalRead(2)==HIGH){
         enter=false;
         i=1000; 
       }
-      delay(1);
+      delay(2);
     }
   }
   else{
   } 
 }
 void motioncheck(){
-  motionpresent = false;
-  freq = 0;
+  motionpresent=false;
+  freq=0;
   for (j=0;j<2000; j++){
     if ((digitalRead(5)==HIGH) or (digitalRead(4)==HIGH)){
       freq=freq+1;
     }
-    if (freq > 250){
-      motionpresent=true;
-    }
-    if (freq > 500){
-      enter=true;
-      j=2000;
-    }
-    if(freq<50){
-      enter=false;
-    }
     delay(5);
-  }   
+  } 
+  if (freq >250){
+     motionpresent=true;
+  }
+  if (freq >500){
+    enter=true;
+  }
+  if (freq <50){
+    enter = false;  
+  }
 }
 void systemoff(){
-  Serial.print("System off");
-  Serial.print(", ");
   digitalWrite(11,LOW);
   digitalWrite(12,LOW);
   digitalWrite(13,LOW);
 }
 
 void systemon(){
-  temp = dht.readTemperature();
+  if (dht.readTemperature()-temp <10 or dht.readTemperature()-temp >-10){
+    temp = dht.readTemperature();
+  }
   Serial.print(temp);
   Serial.print(", ");
   if (temp > 25){
-    Serial.print("Fan on, ");
     digitalWrite(12,HIGH);
     digitalWrite(13,LOW);
+    Serial.print("Heater on");
+    Serial.print(", ");
+    Serial.print("Fan off");
+    Serial.print(", ");
   }
   if (temp < 15){
-    
-    Serial.print("Heater on, ");
     digitalWrite(12,LOW); 
     digitalWrite(13,HIGH);
+    Serial.print("Heater off");
+    Serial.print(", ");
+    Serial.print("Fan on");
+    Serial.print(", ");
   }
   if (temp >=15 and temp<=25)
   {
-    Serial.print("Heater off, Fan off, ");
     digitalWrite(12,LOW);
     digitalWrite(13,LOW);
+    Serial.print("Heater off");
+    Serial.print(", ");
+    Serial.print("Fan off");
+    Serial.print(", ");
+  }
+  if (light-analogRead(lightsensor);<10 or light-analogRead(lightsensor); >-10){
+    light=analogRead(lightsensor);
   }
   light=analogRead(lightsensor);
   Serial.print(light);
   Serial.print(", ");
-  if (light_on = false)
+  if (not light_on)
   {
-    if (light < 200)
-    {
-      Serial.print("Lights on, ");
+    if (light < 200){
       digitalWrite(11,HIGH);
+      light_on = true;
     }
   }
   else{
-    if (light>1000){
-      Serial.print("Lights off, ");
+    if (light>600){
       digitalWrite(11,LOW);
+      light_on = false;
     }
   }
-  
+  if (light_on){
+      Serial.print("Lights on");
+      Serial.print(", ");
+  }
+  else{
+    Serial.print("Lights off");
+    Serial.print(", ");
+  }
 }
 
 
@@ -140,6 +169,18 @@ void loop() {
     exitcheck();
   }
   motioncheck();
+  if (enter){
+    Serial.print("Enter, ");
+  }
+  else{
+    Serial.print("Exited, ");
+  }
+  if (motionpresent){
+    Serial.print("Active, ");
+  }
+  else{
+    Serial.print("Inactive, ");
+  }
   if (motionpresent or enter){
     digitalWrite(LED_BUILTIN,HIGH);
     systemon();
@@ -148,20 +189,7 @@ void loop() {
     digitalWrite(LED_BUILTIN,LOW);
     systemoff();
   }
-
-  if (enter){
-    Serial.print("Entered, ");
-  }
-  else{
-    Serial.print("Exited, ");
-  }
-  if (motionpresent){
-    Serial.print("motion in room, ");
-  }
-  else{
-    Serial.print("No motion, ");
-  }
-  Serial.println("");
-  
-  delay(100);
+  delay(1000);
+  Serial.println();
 }
+
